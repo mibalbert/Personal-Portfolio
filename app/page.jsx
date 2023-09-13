a1cc28d3de6fce9963a62f0e3207a388093d8fd7
Alberto <mib.albert@gmail.com>
2023-09-13 23:22:05 +0300

    JAND

---
+++ app/page.jsx
@@ -0,0 +1,51 @@
+/**
+ * /page.jsx
+ */
+
+import Card from "@/components/Card";
+
+export default function App() {
+  // const [isPresent, safeToRemove] = usePresence();
+  // const [scope, animate] = useAnimate();
+
+  // useEffect(() => {
+  //   if (isPresent) {
+  //     const enterAnimation = async () => {
+  //       await animate(scope.current, { opacity: 1 });
+  //       await animate("li", { opacity: 1, x: 0 });
+  //     };
+  //     enterAnimation();
+  //   } else {
+  //     const exitAnimation = async () => {
+  //       await animate("li", { opacity: 0, x: -100 });
+  //       await animate(scope.current, { opacity: 0 });
+  //       safeToRemove();
+  //     };
+
+  //     exitAnimation();
+  //   }
+  // }, [isPresent]);
+  return (
+    // <div className="pt-[80vh] pb-[50vh] flex flex-col gap-32">
+    <div className="pb-[50vh] ">
+      <div className="h-[100vh] pt-[20vh] flex flex-col gap-10">
+        <div className="text-6xl w-[80%] mx-auto font-bold">
+          This is the fucking header
+        </div>
+        <div className="text-3xl w-[80%] mx-auto">Lorem ipsum dolor sit.</div>
+        <div className="text-xl w-[80%] mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+          consectetur ab facere, sit debitis nulla impedit doloremque velit quod
+          et repudiandae optio esse iure minus exercitationem! Impedit molestiae
+          iste consectetur.
+        </div>
+      </div>
+      <div className="flex flex-col gap-32">
+        <Card className="mx-auto" />
+        <Card className="mx-auto" />
+        <Card className="mx-auto" />
+        <Card className="mx-auto" />
+      </div>
+    </div>
+  );
+}
---
+++ components/Card.jsx
@@ -0,0 +1,115 @@
+/**
+ * Card.jsx
+ */
+
+"use client";
+
+import React, { useRef } from "react";
+import Link from "next/link";
+import { motion, useAnimation, useScroll } from "framer-motion";
+import { useEffect, useState } from "react";
+import { inView } from "framer-motion";
+
+const Card = () => {
+  const [data, setData] = useState("");
+
+  const ref = useRef(null);
+
+  inView(ref.current, () => {
+    console.log("Element has entered the viewport");
+  });
+
+  const videoVariants = {
+    vidoffscreen: {
+      y: 70,
+      opacity: 0.8,
+      scale: 0.9,
+
+      transition: {
+        type: "ease",
+        duration: 1.2,
+      },
+    },
+    vidonscreen: {
+      y: 0,
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "ease",
+        duration: 1.2,
+      },
+    },
+  };
+  const descVariants = {
+    descoffscreen: {
+      y: 70,
+      opacity: 0.8,
+      // scale: 0.9,
+
+      transition: {
+        type: "ease",
+        duration: 1.2,
+      },
+    },
+    desconscreen: {
+      y: 0,
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "ease",
+        duration: 1.6,
+      },
+    },
+  };
+  const op = () => {
+    setData("asdasd");
+
+    return "onscreen";
+  };
+
+  console.log(data);
+  return (
+    <div className="w-[80%] mx-auto grid grid-cols-3 min-h-[70vh] gap-10">
+      <motion.div
+        initial="vidoffscreen"
+        whileInView="vidonscreen"
+        exit="vidoffscreen"
+        variants={videoVariants}
+        viewport={{ once: true, amount: 0.1 }}
+        className=" mx-auto col-span-2"
+        ref={ref}
+      >
+        <video className="w-full rounded-md" loop muted autoPlay>
+          <source src="video.mp4" />
+        </video>
+      </motion.div>
+      <motion.div
+        initial="descoffscreen"
+        whileInView="desconscreen"
+        exit="descoffscreen"
+        variants={descVariants}
+        viewport={{ once: true, amount: 0.1 }}
+        className="col-span-1 flex flex-col gap-4 py-3"
+        ref={ref}
+      >
+        <h1 className="text-3xl font-bold">This is the first Title</h1>
+        <h3 className="text-lg text-neutral-600 font-semibold">
+          React, NextJS, Prisma, Mysql (Planetscale)
+        </h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti,
+          laudantium praesentium facere in neque repellat blanditiis eos
+          assumenda pariatur est.
+        </p>
+        <Link href="https://github.com" className="font-semibold">
+          Github
+        </Link>
+        <Link href="/" className="font-semibold ">
+          View Live
+        </Link>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Card;
